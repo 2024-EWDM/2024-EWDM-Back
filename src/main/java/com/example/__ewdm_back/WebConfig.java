150fa216416f19241f24a2836e207c8363904887
gyesswhat <h2weirdo@icloud.com>
2024-10-21 17:15:17 +0200

    chore: CORS 설정

--- src/main/java/com/example/__ewdm_back/WebConfig.java
+++ src/main/java/com/example/__ewdm_back/WebConfig.java
@@ -9,6 +9,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해 CORS 허용 설정
-                .allowedOrigins("https://2024-ewdm.vercel.app/", "http://192.168.0.6:3000/", "https://2024-ewdm-6y20wzpa7-naeuns-projects-66f9d841.vercel.app/"); // 허용할 origin 목록
+                .allowedOrigins("https://2024-ewdm.vercel.app/", "http://192.168.0.6:3000/", "https://2024-ewdm-6y20wzpa7-naeuns-projects-66f9d841.vercel.app/", "http://localhost:3000/"); // 허용할 origin 목록
     }
 }
