0ee5c15828d7320c6cb3a1fa6a45c924eede9c07
gyesswhat <h2weirdo@icloud.com>
2024-10-21 15:12:07 +0200

    chore: CORS 설정

---
+++ src/main/java/com/example/__ewdm_back/WebConfig.java
@@ -0,0 +1,14 @@
+package com.example.__ewdm_back;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 모든 경로에 대해 CORS 허용 설정
+                .allowedOrigins("https://2024-ewdm.vercel.app/", "http://192.168.0.6:3000/"); // 허용할 origin 목록
+    }
+}
